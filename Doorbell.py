70a69386f09595f86f51130bf6b289193faf4006
ssweall <44494824+ssweall@users.noreply.github.com>
2022-10-20 14:21:56 +0200

    Add files via upload

---
+++ Doorbell.py
@@ -0,0 +1,19 @@
+import time
+import RPi.GPIO as GPIO
+import subprocess
+
+BUTTON_PIN = 26
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(BUTTON_PIN, GPIO.IN)
+
+while True :
+    etat = GPIO.input(BUTTON_PIN)
+
+    if (etat == 0) :
+        subprocess.Popen(["motion", "-c", "motion-mmalcam-both.conf"])
+        time.sleep(60)
+        subprocess.Popen(["killall", "motion"])
+        time.sleep(3)
+    time.sleep(0.3)
